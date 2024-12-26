a7cce7a782d1da40e2101bacdde7c96749315610
Rahima-Akter <rahimaakter1527@gmail.com>
2024-12-25 21:46:54 -0800

    register functionality with firebase added

--- src/Utils/router.jsx
+++ src/Utils/router.jsx
@@ -24,6 +24,7 @@ const router = createBrowserRouter([
         children: [
             {
                 path: '/',
+                element: <Home />,
             },
             {
                 path: '/allItem',
